b45c09448799130d5447e966950dfc295f8599c1
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-01-09 11:40:28 +0100

    6. feladatig kész

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 
 interface WeatherData {
   temperature: number,
@@ -9,11 +9,41 @@ interface WeatherData {
 
 export default function Index() {
 
+  const getWeather = async() => {
+    try {
+      const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=47.9026&longitude=20.3733&current_weather=true");
+      const data = await response.json();
+      console.log(data);
+      setWeather(data.current_weather);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getWeather();
+  },[]);
+
   const [weather,setWeather] = useState<WeatherData | null>(null);
   const [loading,setLoading] = useState<boolean>(true);
 
   return (
     <View>
+      {
+        loading ? <ActivityIndicator /> 
+                : 
+          weather ? (
+            <View>
+              <Text>Eger időjárása</Text>
+              <Text>Hőmérséklet: {weather.temperature}°C</Text>
+              <Text>Szélsebesség: {weather.windspeed} km/h</Text>
+              <Text>Szélirány: {weather.winddirection}°</Text>
+            </View>
+          )
+          :( <Text>Nem sikerült az időjárás adatot letölteni!</Text> )
+      }
     </View>
   );
 }
